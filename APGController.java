2f3af1a9ac16d29dfc3c1637cf81151a28c01fb5
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-14 18:31:50 -0400

    Linked UI elements with APGController

--- APGController.java
+++ APGController.java
@@ -1,6 +1,22 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Jiachen on 3/14/18.
  * Auto Paragraph Generator Controller
  */
 public class APGController {
+
+    public TextField delimiter;
+    public TextField subjectArea;
+    public CheckBox sentenceShuffling;
+    public CheckBox synReplacement;
+    public CheckBox citeSources;
+    public CheckBox crsCtxWordSwapping;
+    public CheckBox posSpecSynMapping;
+    public TextArea topics;
+    public Button generate;
+    public TextArea generatedPars;
 }
