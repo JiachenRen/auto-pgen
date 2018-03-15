7c6d60d2d4b653dfa823dff72b2b4162648293dc
Jiachen Ren <arc4jiachenren@gmail.com>
2018-03-14 20:09:10 -0400

    Threw time-consuming web-crawling tasks into the background. The app is now finished.

--- APGController.java
+++ APGController.java
@@ -1,6 +1,10 @@
 import components.Extractor;
 import components.Generator;
 import components.Item;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
@@ -29,31 +33,26 @@ public class APGController {
     public TextArea generatedPars;
     public ScrollBar minNumSentences;
     public ScrollBar maxNumSentences;
+    public Label leftStatus;
+    public Label minLabel;
+    public Label maxLabel;
 
     private Generator generator;
 
     void initListeners(Generator generator) {
         this.generator = generator;
         l("Initializing event listeners... ");
-        sentenceShuffling.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            generator.setShuffleSentences(newValue);
-        });
-        synReplacement.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            generator.setSwapWords(newValue);
-        });
-        citeSources.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            generator.setIncludeSources(newValue);
-        });
-        crsCtxWordSwapping.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            generator.setCrossContextWordSwapping(newValue);
-        });
-        posSpecSynMapping.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            generator.setPosSpecificSynMapping(newValue);
-        });
+        sentenceShuffling.selectedProperty().addListener((observable, oldValue, newValue) -> generator.setShuffleSentences(newValue));
+        synReplacement.selectedProperty().addListener((observable, oldValue, newValue) -> generator.setSwapWords(newValue));
+        citeSources.selectedProperty().addListener((observable, oldValue, newValue) -> generator.setIncludeSources(newValue));
+        crsCtxWordSwapping.selectedProperty().addListener((observable, oldValue, newValue) -> generator.setCrossContextWordSwapping(newValue));
+        posSpecSynMapping.selectedProperty().addListener((observable, oldValue, newValue) -> generator.setPosSpecificSynMapping(newValue));
+        minNumSentences.valueProperty().addListener((observable, oldValue, newValue) -> minLabel.setText(String.valueOf(minNumSentences.getValue())));
+        maxNumSentences.valueProperty().addListener((observable, oldValue, newValue) -> maxLabel.setText(String.valueOf(maxNumSentences.getValue())));
     }
 
     public void generateButtonPressed(MouseEvent mouseEvent) {
-        String topics[], output = "";
+        String topics[];
         String delimiter = this.delimiter.getText();
         String topicsRaw = this.topics.getText();
         if (!delimiter.equals("")) topics = topicsRaw.split(delimiter);
@@ -61,22 +60,29 @@ public class APGController {
         String subjectArea = this.subjectArea.getText();
         int minSentences = (int) this.minNumSentences.getValue();
         int maxSentences = (int) this.maxNumSentences.getValue();
+        generatedPars.setText("");
 
-        for (String topic : topics) {
-            if (topic.endsWith("\n")) topic = topic.substring(0, topic.length() - 1);
-            String rawContent = Extractor.search(topic + (subjectArea.equals(" ") ? "" : subjectArea));
-            ArrayList<Item> items = Item.extractItemsFrom(rawContent);
-            int numSentences = (int) (minSentences + Math.random() * (maxSentences - minSentences + 1));
-            String paragraph = generator.generateSimpleParagraph(items, numSentences);
-//            boldYellow.println(topic);
-//            magenta.println(paragraph);
-//            l("\n");
-            output += topic + "\n" + paragraph + "\n";
+        String output = "";
+        for (String t : topics) {
+            final String[] topic = {t};
+            Task<String> task = new Task<String>() {
+                @Override
+                public String call() throws Exception {
+                    if (topic[0].endsWith("\n")) topic[0] = topic[0].substring(0, topic[0].length() - 1);
+                    String rawContent = Extractor.search(topic[0] + (subjectArea.equals(" ") ? "" : subjectArea));
+                    ArrayList<Item> items = Item.extractItemsFrom(rawContent);
+                    int numSentences = (int) (minSentences + Math.random() * (maxSentences - minSentences + 1));
+                    return generator.generateSimpleParagraph(items, numSentences);
+                }
+            };
+            task.setOnSucceeded(event -> {
+                leftStatus.setText("Working on \"" + topic[0] + "\" ...");
+                String result = event.getSource().getValue().toString();
+                String updatedContent = generatedPars.getText() + topic[0] + "\n" + result + "\n\n";
+                generatedPars.setText(updatedContent);
+            });
+            new Thread(task).start();
         }
-        generatedPars.setText(output);
-
-        boldGreen.println("Writing files...");
-        Extractor.write("output", "generated", output);
     }
 
     private void l(Object... os) {
